e0d07d80037f1778e4c6285d3c0a00a75c335675
Anikesh <developeranikesh@gmail.com>
2021-12-17 11:15:30 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # url-shortener-ft-socket
+
+#### Run
+
+```bash
+npm install
+```
+
+```bash
+npm start # then open localhost:8081 in browser
+```
--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = 8081
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
